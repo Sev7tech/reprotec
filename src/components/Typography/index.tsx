d23715aafcdcf9880e9fafdf4d17ebb6714c219c
Sev7tech <sev7tech@gmail.com>
2025-01-20 21:32:39 -0300

    chore(typography): add BlueTitle for highline title

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -4,6 +4,7 @@ import CustomLink from './CustomLinks'
 import Text from './Text'
 import H3 from './H3'
 import H2 from './H2'
+import { BlueTitle } from './BlueTitle'
 
 export interface ITypographyBase {
   children: ReactNode
@@ -14,5 +15,6 @@ export const Typography = {
   Link: CustomLink,
   Text,
   H2,
-  H3
+  H3,
+  BlueTitle
 }
