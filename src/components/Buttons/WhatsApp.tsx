fddfd0d8c75f228aa519f51f951c62784fe1e854
Sev7tech <sev7tech@gmail.com>
2025-01-21 14:13:11 -0300

    feat(buttons): implement WhatsApp button

---
+++ src/components/Buttons/WhatsApp.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+
+import Picture from '../Picture'
+
+export const WhatsApp = () => {
+  const buttonTitle = 'Entrar em contato pelo WhatApp'
+
+  return (
+    <Link
+      href={`https://wa.me/${'phoneNumber'}?text=${encodeURIComponent('message')}`}
+      title={buttonTitle}
+      className="fixed bottom-4 right-4 z-30 h-12 w-12 rounded-full bg-emerald-500 p-2 md:h-14 md:w-14"
+      target="_blank"
+    >
+      <Picture
+        src="/images/icons/whatsapp.svg"
+        alt={buttonTitle}
+        $w={56}
+        $h={56}
+      />
+    </Link>
+  )
+}
