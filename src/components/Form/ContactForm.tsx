7f3de2ac42fc5a11124e77ed78b716d41b388391
Sev7tech <sev7tech@gmail.com>
2025-01-22 16:03:17 -0300

    feat(form): add WorkForm component and enhance form handling
    - Added `WorkForm` component with API integration for sending form data via email.
    - Implemented validation for the file input field to ensure proper file handling.
    - Refactored input classes: moved styles from individual inputs to global styles for better maintainability.
    - Introduced reusable form elements (`Input`, `Label`, `InputGroup`, `PhoneMask`) to streamline form development.
    
    BREAKING CHANGE: Some input-specific styles were removed in favor of global styles. Ensure all forms are reviewed for compatibility.
    
    Related files:
    - New components: `ContactForm`, `WorkForm`, and form element utilities.
    - Pending updates: `Popup` component remains untracked and will be handled in a separate commit.
    
    This update improves form scalability, reusability, and consistency across the application.

---
+++ src/components/Form/ContactForm.tsx
@@ -0,0 +1,169 @@
+'use client'
+
+import { Field, Formik } from 'formik'
+import { memo, useState } from 'react'
+import * as Yup from 'yup'
+
+import { FormElement } from './FormElements'
+import { Typography } from '../Typography'
+import { Buttons } from '../Buttons'
+import Popup from '../Popup'
+
+import sendForm from '@/api/SendForm'
+
+const validationSchema = Yup.object({
+  firstName: Yup.string()
+    .min(2, 'O nome deve ter no mínimo 2 caracteres.')
+    .required('Campo obrigatório'),
+  lastName: Yup.string()
+    .min(2, 'O sobrenome deve ter no mínimo 2 caracteres.')
+    .required('Campo obrigatório.'),
+  whatsapp: Yup.string()
+    .matches(
+      /^\(?\d{2}\)?[\s-]?\d{4,5}[-]?\d{4}$/,
+      'Número inválido, deve ser um telefone.'
+    )
+    .required('Campo obrigatório.'),
+  email: Yup.string().email('E-mail inválido.').required('Campo obrigatório.'),
+  message: Yup.string()
+    .min(20, 'Descreva sua mensagem para que possamos te ajudar.')
+    .required('Campo obrigatório.'),
+  terms: Yup.boolean()
+    .oneOf([true], 'Você tem que aceitar os termos para proseguir.')
+    .required('Campo obrigatório.')
+})
+
+const ContactForm = memo(() => {
+  const [isPopupVisible, setIsPopupVisible] = useState(false)
+
+  return (
+    <>
+      <Formik
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          whatsapp: '',
+          email: '',
+          message: '',
+          terms: false
+        }}
+        validationSchema={validationSchema}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          sendForm({
+            subject: 'Contato Recebido - Site Reprotec',
+            text: `Olá, equipe Reprotec! \n
+          Recebemos um novo contato do site. Seguem os dados: \n`,
+            html: `<p><strong>Nome: </strong>${values.firstName}</p>
+      <p><strong>Sobrenome: </strong>${values.lastName}</p>
+      <p><strong>Link do WhatsApp: </strong> <a href="https://wa.me/55${values.whatsapp}">Click aqui para conversar no WhatsApp</a></p>
+      <p><strong>Telefone: </strong>+55 ${values.whatsapp}</p>
+      <p><strong>E-mail: </strong>${values.email}</p>
+      <p><strong>Mensagem: </strong>${values.message}</p>
+      <p>Atenciosamente,</p>
+      <p>Equipe Reprotec.</p>`
+          })
+            .then(() => {
+              resetForm()
+              setSubmitting(false)
+              setIsPopupVisible(true)
+            })
+            .catch((error) => alert(`Erro ao enviar o formulário ${error}`))
+            .finally(() => {
+              resetForm()
+              setSubmitting(false)
+            })
+        }}
+      >
+        {({ handleSubmit, isSubmitting }) => (
+          <form
+            onSubmit={handleSubmit}
+            className="gapCol8 w-full rounded-2xl bg-primaryBlue/10 p-4"
+          >
+            <FormElement.InputGroup>
+              <FormElement.Label htmlFor="firstName">
+                Primeiro Nome*
+              </FormElement.Label>
+              <Field
+                type="text"
+                name="firstName"
+                id="firstName"
+                component={FormElement.Input}
+              />
+            </FormElement.InputGroup>
+
+            <FormElement.InputGroup>
+              <FormElement.Label htmlFor="lastName">
+                Sobrenome*
+              </FormElement.Label>
+              <Field
+                type="text"
+                name="lastName"
+                id="lastName"
+                component={FormElement.Input}
+              />
+            </FormElement.InputGroup>
+
+            <FormElement.InputGroup>
+              <FormElement.Label htmlFor="whatsapp">
+                WhatsApp*
+              </FormElement.Label>
+              <Field
+                type="text"
+                name="whatsapp"
+                id="whatsapp"
+                component={FormElement.MaskedInputComponent}
+              />
+            </FormElement.InputGroup>
+
+            <FormElement.InputGroup>
+              <FormElement.Label htmlFor="email">Email*</FormElement.Label>
+              <Field
+                type="email"
+                name="email"
+                id="email"
+                component={FormElement.Input}
+              />
+            </FormElement.InputGroup>
+
+            <FormElement.InputGroup>
+              <FormElement.Label htmlFor="message">Mensagem*</FormElement.Label>
+              <Field
+                type="textarea"
+                name="message"
+                id="message"
+                component={FormElement.Input}
+                isTextarea
+              />
+            </FormElement.InputGroup>
+
+            <FormElement.InputGroup className="flex place-items-baseline gap-2">
+              <Field
+                type="checkbox"
+                name="terms"
+                id="terms"
+                component={FormElement.Input}
+                isCheckBox
+              />
+
+              <FormElement.Label htmlFor="terms">
+                Eu concordo com a{' '}
+                <Typography.Link
+                  href="/politica-privacidade"
+                  linkTitle="Ler Politica de privacidade"
+                  className="underline"
+                >
+                  política de privacidade
+                </Typography.Link>
+              </FormElement.Label>
+            </FormElement.InputGroup>
+
+            <Buttons.Submit isSending={isSubmitting} />
+          </form>
+        )}
+      </Formik>
+      {isPopupVisible && <Popup isContactForm />}
+    </>
+  )
+})
+
+export default ContactForm
---
+++ src/components/Form/WorkForm.tsx
@@ -0,0 +1,192 @@
+'use client'
+
+import { Field, Formik } from 'formik'
+import { memo, useState } from 'react'
+import * as Yup from 'yup'
+
+import { FormElement } from './FormElements'
+import { Typography } from '../Typography'
+import { Buttons } from '../Buttons'
+import Popups from '../Popup'
+
+import sendForm from '@/api/SendForm'
+
+const validationSchema = Yup.object({
+  firstName: Yup.string()
+    .min(2, 'O nome deve ter no mínimo 2 caracteres.')
+    .required('Campo obrigatório'),
+  lastName: Yup.string()
+    .min(2, 'O sobrenome deve ter no mínimo 2 caracteres.')
+    .required('Campo obrigatório.'),
+  whatsapp: Yup.string()
+    .matches(
+      /^\(?\d{2}\)?[\s-]?\d{4,5}[-]?\d{4}$/,
+      'Número inválido, deve ser um telefone.'
+    )
+    .required('Campo obrigatório.'),
+  email: Yup.string().email('E-mail inválido.').required('Campo obrigatório.'),
+  file: Yup.mixed<File>()
+    .test('Size file', 'O arquivo pode ter no máximo 2MB.', (value) => {
+      return value && value.size <= 2 * 1024 * 1024
+    })
+    .required('Campo obrigatório.'),
+  terms: Yup.boolean()
+    .oneOf([true], 'Você tem que aceitar os termos para proseguir.')
+    .required('Campo obrigatório.')
+})
+
+const WorkForm = memo(() => {
+  const [isPopupVisible, setIsPopupVisible] = useState(false)
+
+  return (
+    <>
+      <Formik
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          whatsapp: '',
+          email: '',
+          file: null,
+          terms: false
+        }}
+        validationSchema={validationSchema}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          sendForm({
+            subject: 'Candidatura Recebida - Site Reprotec',
+            text: `Olá, equipe Reprotec! \n
+          Recebemos uma nova candidatura de emprego no site. Seguem os dados: \n`,
+            html: `<p><strong>Nome: </strong>${values.firstName}</p>
+      <p><strong>Sobrenome: </strong>${values.lastName}</p>
+      <p><strong>Link do WhatsApp: </strong> <a href="https://wa.me/55${values.whatsapp}">Click aqui para conversar no WhatsApp</a></p>
+      <p><strong>Telefone: </strong>+55 ${values.whatsapp}</p>
+      <p><strong>E-mail: </strong>${values.email}</p>
+      <p><strong>currículo: </strong>${values.file}</p>
+      <p>Atenciosamente,</p>
+      <p>Equipe Reprotec.</p>`
+          })
+            .then(() => {
+              resetForm()
+              setSubmitting(false)
+              setIsPopupVisible(true)
+            })
+            .catch((error) => alert(`Error ao enviar o formulário ${error}`))
+            .finally(() => {
+              resetForm()
+              setSubmitting(false)
+            })
+        }}
+      >
+        {({ handleSubmit, values, isSubmitting }) => (
+          <form
+            onSubmit={handleSubmit}
+            className="gapCol8 w-full rounded-2xl bg-primaryBlue/10 p-4"
+          >
+            <FormElement.InputGroup>
+              <FormElement.Label htmlFor="firstName">
+                Primeiro Nome*
+              </FormElement.Label>
+              <Field
+                type="text"
+                name="firstName"
+                id="firstName"
+                component={FormElement.Input}
+              />
+            </FormElement.InputGroup>
+
+            <FormElement.InputGroup>
+              <FormElement.Label htmlFor="lastName">
+                Sobrenome*
+              </FormElement.Label>
+              <Field
+                type="text"
+                name="lastName"
+                id="lastName"
+                component={FormElement.Input}
+              />
+            </FormElement.InputGroup>
+
+            <FormElement.InputGroup>
+              <FormElement.Label htmlFor="whatsapp">
+                WhatsApp*
+              </FormElement.Label>
+              <Field
+                type="text"
+                name="whatsapp"
+                id="whatsapp"
+                component={FormElement.MaskedInputComponent}
+              />
+            </FormElement.InputGroup>
+
+            <FormElement.InputGroup>
+              <FormElement.Label htmlFor="email">Email*</FormElement.Label>
+              <Field
+                type="email"
+                name="email"
+                id="email"
+                component={FormElement.Input}
+              />
+            </FormElement.InputGroup>
+
+            <FormElement.InputGroup>
+              <Field name="file">
+                {({ field, form }: any) => (
+                  <>
+                    <input
+                      className="hidden"
+                      type="file"
+                      id="file"
+                      accept=".pdf"
+                      onChange={(
+                        event: React.ChangeEvent<HTMLInputElement>
+                      ) => {
+                        const file = event.currentTarget.files?.[0]
+                        form.setFieldValue(field.name, file)
+                      }}
+                    />
+                    <FormElement.Label
+                      htmlFor="file"
+                      className="mt-4 w-[200px] cursor-pointer overflow-hidden rounded-md bg-midnightBlue p-2 text-center text-white"
+                    >
+                      {!values.file
+                        ? 'Anexar Currículo (PDF)*'
+                        : `arquivo ${field.value.name}`}
+                    </FormElement.Label>
+                    <span className="message-error">
+                      {form.errors[field.name] as string}
+                    </span>
+                  </>
+                )}
+              </Field>
+            </FormElement.InputGroup>
+
+            <FormElement.InputGroup className="flex place-items-baseline gap-2">
+              <Field
+                type="checkbox"
+                name="terms"
+                id="terms"
+                component={FormElement.Input}
+                isCheckBox={true}
+              />
+
+              <FormElement.Label htmlFor="terms">
+                Eu concordo com a{' '}
+                <Typography.Link
+                  href="/politica-privacidade"
+                  linkTitle="Ler Politica de privacidade"
+                  className="underline"
+                >
+                  política de privacidade
+                </Typography.Link>
+              </FormElement.Label>
+            </FormElement.InputGroup>
+
+            <Buttons.Submit isSending={isSubmitting} />
+          </form>
+        )}
+      </Formik>
+      {isPopupVisible && <Popups isContactForm={false} />}
+    </>
+  )
+})
+
+export default WorkForm
