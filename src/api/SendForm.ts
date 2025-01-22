883ffbef1885fa6ce2f14a3b56209dcc603a7d59
Sev7tech <sev7tech@gmail.com>
2025-01-22 15:57:22 -0300

    feat(api): implement api for send the form to email

---
+++ src/api/SendForm.ts
@@ -0,0 +1,36 @@
+'use server'
+
+import nodemailer from 'nodemailer'
+import SMTPTransport from 'nodemailer/lib/smtp-transport'
+
+import { ISendForm } from '@/types'
+
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: process.env.EMAIL_SECURE !== 'development',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD
+  }
+} as SMTPTransport.Options)
+
+async function sendForm({ subject, text, html }: ISendForm) {
+  try {
+    const info = await transporter.sendMail({
+      from: `Site Reprotec: <${process.env.EMAIL_USER}>`,
+      to: process.env.EMAIL_RECEIVER,
+      subject,
+      text,
+      html
+    })
+
+    return info
+  } catch (error) {
+    throw new Error(`${error}`)
+  } finally {
+    transporter.close()
+  }
+}
+
+export default sendForm
