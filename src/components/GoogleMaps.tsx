776429780273508cd389edf5d095c53cc8dc07e5
Sev7tech <sev7tech@gmail.com>
2025-01-23 01:08:07 -0300

    feat: implement GoogleMaps component for display Reprotec address

---
+++ src/components/GoogleMaps.tsx
@@ -0,0 +1,14 @@
+const GoogleMaps = () => {
+  return (
+    <iframe
+      title="Localização da Reprotec"
+      width="100%"
+      height="500"
+      src="https://maps.google.com/maps?width=100%25&amp;height=500&amp;hl=pt&amp;q=Avenida%20Venturosa,%20111Jd.%20Cumbica%20Guarulhos/%20SP07240-000%20+(Reprotec)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+    >
+      <a href="https://www.gps.ie/">gps</a>
+    </iframe>
+  )
+}
+
+export default GoogleMaps
