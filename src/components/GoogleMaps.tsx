c0149f2ca85867927c8cb197f28fb173e356052a
Sev7tech <sev7tech@gmail.com>
2025-01-23 01:14:01 -0300

    style: update height for maps

--- src/components/GoogleMaps.tsx
+++ src/components/GoogleMaps.tsx
@@ -3,8 +3,8 @@ const GoogleMaps = () => {
     <iframe
       title="Localização da Reprotec"
       width="100%"
-      height="500"
-      src="https://maps.google.com/maps?width=100%25&amp;height=500&amp;hl=pt&amp;q=Avenida%20Venturosa,%20111Jd.%20Cumbica%20Guarulhos/%20SP07240-000%20+(Reprotec)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+      height="600"
+      src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=pt&amp;q=Avenida%20Venturosa,%20111Jd.%20Cumbica%20Guarulhos/%20SP07240-000%20+(Reprotec)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
     >
       <a href="https://www.gps.ie/">gps</a>
     </iframe>
