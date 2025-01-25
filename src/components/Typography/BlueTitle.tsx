b3645d262c5512eb3dd4ed36344b9895a6139a93
sev7tech <sev7tech@gmail.com>
2025-01-25 19:58:42 -0300

    style: update color for accesiblity

--- src/components/Typography/BlueTitle.tsx
+++ src/components/Typography/BlueTitle.tsx
@@ -2,8 +2,9 @@ import { ITypographyBase } from '.'
 
 export const BlueTitle = ({ children }: ITypographyBase) => {
   return (
-    <h2 className="flex items-center text-xl font-semibold text-cyan-600">
-      <div className="mr-2 w-6 border-b-2 border-cyan-600"></div> {children}
+    <h2 className="flex items-center text-xl font-bold text-primaryBlue/80">
+      <div className="mr-2 w-6 border-b-2 border-primaryBlue/80"></div>{' '}
+      {children}
     </h2>
   )
 }
