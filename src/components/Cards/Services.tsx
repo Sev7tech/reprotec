184cbb25cbea65e44d9693cd5ac88d30ae2d7cfa
sev7tech <sev7tech@gmail.com>
2025-01-26 13:05:00 -0300

    feat(cards): add attribute isSeoTitle for conditional isSeoTitle - is true handle h2 - is false handle h3 - default is false

--- src/components/Cards/Services.tsx
+++ src/components/Cards/Services.tsx
@@ -10,13 +10,20 @@ const ServicesCard = ({
   text,
   href,
   title,
-  children
+  children,
+  isSeoTitle = false
 }: IServiceCard) => {
+  const Title = isSeoTitle ? (
+    <h2 className="text-2xl font-bold text-white">{cardTitle}</h2>
+  ) : (
+    <Typography.H3 className="text-white">{cardTitle}</Typography.H3>
+  )
+
   return (
     <article className="overflow-hidden rounded-xl bg-primaryBlue shadow-lg duration-300 hover:scale-105 hover:brightness-105">
       <Picture src={src} alt={alt} $w={580} $h={400} />
       <div className="gapCol16 p-4">
-        <Typography.H3 className="text-white">{cardTitle}</Typography.H3>
+        {Title}
         <Typography.Text className="text-white">{text}</Typography.Text>
         <Typography.Link href={href} linkTitle={title} isTextWhite>
           entrar em contato <span className="text-for-seo">{children}</span>
