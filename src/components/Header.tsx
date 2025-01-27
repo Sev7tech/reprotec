c235698df9a385bd0a132226c7ec4067bfb811ad
sev7tech <sev7tech@gmail.com>
2025-01-27 15:26:12 -0300

    fix: update class for remove scroll-x

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,7 +18,7 @@ const Header = memo(() => {
       <div className="max-container flexBetween w-95 relative py-4 md:py-2">
         <Brand src="/images/logo-white.png" />
         <nav
-          className={`${menuOpen ? 'translate-x-0 bg-blue-950 opacity-100 ' : '-translate-x-full opacity-0 lg:translate-x-0 lg:opacity-100'} mainNav`}
+          className={`${menuOpen ? 'translate-x-0 bg-blue-950 opacity-100' : '-translate-x-full opacity-0 lg:translate-x-0 lg:opacity-100'} mainNav`}
           role="navigation"
           aria-label={'mainNavigation'}
         >
