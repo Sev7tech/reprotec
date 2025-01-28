5455335ff61bb7776984e93ae84c2c2d443db868
sev7tech <sev7tech@gmail.com>
2025-01-28 09:32:38 -0300

    fix: update class for remove scroll x

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,15 +14,15 @@ const Header = memo(() => {
   const toggleMenu = () => setMenuOpen(!menuOpen)
 
   return (
-    <header className="fixed top-0 z-50 w-full bg-midnightBlue lg:w-full">
-      <div className="max-container flexBetween w-95 relative py-4 md:py-2">
+    <header className="fixed top-0 z-50 w-screen bg-midnightBlue md:w-full">
+      <div className="max-container flexBetween w-95 relative py-4 md:py-2 lg:w-[98%]">
         <Brand src="/images/logo-white.png" />
         <nav
           className={`${menuOpen ? 'translate-x-0 bg-blue-950 opacity-100' : '-translate-x-full opacity-0 lg:translate-x-0 lg:opacity-100'} mainNav`}
           role="navigation"
           aria-label={'mainNavigation'}
         >
-          <ul className="gapCol24 lg:flexRow pt-4 lg:space-x-4 lg:p-0">
+          <ul className="gapCol24 lg:flexRow relative h-screen pt-4 md:h-auto lg:space-x-4 lg:p-0">
             {navigatorLinks.map((link) => (
               <li key={link.id}>
                 <Typography.Link
