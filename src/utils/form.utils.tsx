a426504320cb066668bd13a8e9a529dcb91bf3fa
sev7tech <sev7tech@gmail.com>
2025-01-28 14:35:27 -0300

    fix: add corret env for secure

--- src/utils/form.utils.tsx
+++ src/utils/form.utils.tsx
@@ -9,7 +9,7 @@ const transporter = nodemailer.createTransport({
   // service: 'Gmail',
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
-  secure: process.env.NODE_ENV !== 'development',
+  secure: process.env.EMAIL_SECURE,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD
